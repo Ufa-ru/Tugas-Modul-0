59c512639fb4edb1272554cf44afa445a6bcbac1
Muhammad Naufal Abdillah <naufaldillah27@gmail.com>
2024-12-06 18:05:32 +0800

    Delete Modul 2/Modul 2 Bahasa C directory

--- Modul 2/Modul 2 Bahasa C/Modul 2 Bahasa Python/PRAK202-2410817310007-MuhammmadNaufalAbdillah.py
+++
@@ -1,7 +0,0 @@
-P=float(input("masukan NIlai Pertama :"))
-K=float(input("Masukan Nilai kedua :"))
-hasil=P+K
-if P.is_integer():
-    print(f"Hasil dari penjumlahan nilai pertama \"{int(P)}\" dan nilai kedua \"{K:.1f}\" adalah \"{hasil:.2f}\" ")
-else:
-    print(f"Hasil dari penjumlahan nilai pertama \"{P:.2f}\" dan nilai kedua \"{K:.1f}\" adalah \"{hasil:.2f}\" ")
