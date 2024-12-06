2cfba79030a64524f6fc4e5ce925673224e6442e
Muhammad Naufal Abdillah <naufaldillah27@gmail.com>
2024-12-06 18:11:39 +0800

    Delete modul 1 directory

--- modul 1/Modul 1 Bahasa python/PRAK101-2410817310007-MuhammmadNaufalAbdillah.py
+++
@@ -1,8 +0,0 @@
-x = 5
-y = 7
-z = 9
-Jumlah = x + y + z
-print("Variabel y benilai", y)
-print("Variabel x benilai", x)
-print("Variabel z benilai", z)
-print("Jumlah Variabel tersebut adalah", Jumlah)
--- modul 1/Modul 1 Bahasa python/PRAK104-2410817310007-MuhammmadNaufalAbdillah.py
+++
@@ -1,6 +0,0 @@
-Sepatu_A = 400000
-Sepatu_B = 350000
-print("Harga sepatu A adalah", Sepatu_A)
-print("Harga sepatu A adalah", Sepatu_B)
-print("Sepatu A mendapatkan diskon sebesar 13% hingga harganya menjadi", int(Sepatu_A - (Sepatu_A * 13/100)))
-print("Sepatu B mendapatkan diskon sebesar 21% hingga harganya menjadi", int(Sepatu_B - (Sepatu_B * 21/100)))
