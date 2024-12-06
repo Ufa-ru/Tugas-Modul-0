ba47a7f38a4ed74286f391e06b0c1829ca484260
Muhammad Naufal Abdillah <naufaldillah27@gmail.com>
2024-12-06 18:11:52 +0800

    Delete modul 3 directory

--- modul 3/Modul 3 Bahasa C/PRAK303-2410817310007-MuhammadNaufalAbdillah.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int Nomor;
-    scanf ("%d", &Nomor);
-    if (Nomor > 0) printf("Positif");
-    else if (Nomor < 0) printf ("Negatif");
-    else printf ("Nol");
-    return 0;
-}
--- modul 3/Modul 3 Bahasa Python/PRAK303-2410817310007-MuhammadNaufalAbdillah.py
+++
@@ -1,4 +0,0 @@
-Nomor = int(input())
-if Nomor > 0: print("Positif")
-elif Nomor < 0: print("Negatif")
-else: Nomor = 0; print("Nol")
